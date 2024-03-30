4f3f7b4fb499768a0f5c4e3bfbc919fb74d54d06
Gonzalo Bazán <gonzalo.bazan.b@hotmail.com>
2024-03-29 20:25:14 -0500

    make it better

--- server.js
+++ server.js
@@ -1,38 +1,55 @@
-// server.js
-
+require('dotenv').config();
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const fs = require('fs');
-const path = require('path');
+const sequelize = require('./config/dbConfig');
+const Herramienta = require('./models/Herramienta');
 
 const app = express();
 
-// Ruta al archivo JSON de cookies
-const cookiesFilePath = path.join(__dirname, 'cookies.json');
-
-// Lee las cookies del archivo JSON
-let cookies = {};
-try {
-    cookies = JSON.parse(fs.readFileSync(cookiesFilePath, 'utf8'));
-} catch (error) {
-    console.error('Error al leer el archivo de cookies JSON:', error);
+async function connectToDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexión a la base de datos establecida con éxito');
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+    }
 }
 
-// Configuración del middleware de proxy
+// Middleware personalizado para leer cookies de la base de datos
+async function leerCookies(req, res, next) {
+    try {
+        const herramienta = await Herramienta.findOne({ where: { id: 2 } });
+        if (herramienta) {
+            const cookiesCadena = herramienta.cookies;
+            req.cookiesDB = cookiesCadena.split('; ').reduce((acumulador, cookie) => {
+                const [clave, valor] = cookie.split('=');
+                acumulador[clave] = valor;
+                return acumulador;
+            }, {});
+        }
+    } catch (error) {
+        console.error('Error al obtener cookies:', error);
+        req.cookiesDB = {};
+    }
+    next();
+}
+
+app.use(leerCookies); // Usa el middleware personalizado
+
 app.use('/', createProxyMiddleware({
-    target: 'https://es.semrush.com', // URL del sitio destino
+    target: 'https://es.semrush.com',
     changeOrigin: true,
     onProxyReq: (proxyReq, req, res) => {
-        // Construir la cadena de cookies
-        const cookieHeader = Object.entries(cookies).map(([key, value]) => `${key}=${value}`).join('; ');
-        // Establecer las cookies en el encabezado de la solicitud proxy
-        proxyReq.setHeader('Cookie', cookieHeader);
+        if (req.cookiesDB) {
+            const cookieHeader = Object.entries(req.cookiesDB).map(([key, value]) => `${key}=${value}`).join('; ');
+            proxyReq.setHeader('Cookie', cookieHeader);
+        }
     },
 }));
 
-// Iniciar el servidor
-const PORT = process.env.PORT || 3000; // Usa el puerto proporcionado por Heroku o, en su defecto, el puerto 3000
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+connectToDatabase();
