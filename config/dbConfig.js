56fff3ca13c17753d52eb49f9da62884afca58d7
Gonzalo Bazán <gonzalo.bazan.b@hotmail.com>
2024-03-29 21:53:40 -0500

    make it better

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.JAWSDB_URL, {
   dialect: 'mysql',
 });
 
