e5b8e2ef0122dcd5891f8546d9e4b1fa1af115fb
Gonzalo Bazán <gonzalo.bazan.b@hotmail.com>
2024-04-24 09:52:27 -0500

    Mensaje de confirmación

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ sequelize.sync()
         console.error('Error al sincronizar la tabla Usuarios:', error);
     });
 
-// Middleware personalizado para leer cookies de la base de datos
+// Middleware para leer cookies de la base de datos
 async function leerCookies(req, res, next) {
     try {
         const herramienta = await Herramienta.findOne({ where: { id: 2 } });
         if (herramienta) {
             const cookiesCadena = herramienta.cookies;
-            console.log(cookiesCadena)
+            console.log(cookiesCadena);
+            // Transforma la cadena de cookies en un objeto
             req.cookiesDB = cookiesCadena.split('; ').reduce((acumulador, cookie) => {
                 const [clave, valor] = cookie.split('=');
                 acumulador[clave] = valor;
@@ -43,15 +44,16 @@ async function leerCookies(req, res, next) {
     next();
 }
 
-app.use(leerCookies); // Usa el middleware personalizado
+app.use(leerCookies); // Uso del middleware personalizado
 
 app.use('/', createProxyMiddleware({
-    target: 'https://es.semrush.com',
+    target: 'https://es.semrush.com/', // Asegúrate de que esta URL es correcta y accesible
     changeOrigin: true,
     onProxyReq: (proxyReq, req, res) => {
         if (req.cookiesDB) {
             const cookieHeader = Object.entries(req.cookiesDB).map(([key, value]) => 
                 `${key}=${encodeURIComponent(value)}`).join('; ');
+            console.log('Enviando Cookie Header:', cookieHeader); // Añade esto para verificar
             proxyReq.setHeader('Cookie', cookieHeader);
         }
     },
